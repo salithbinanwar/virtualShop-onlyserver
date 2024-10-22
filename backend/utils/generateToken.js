105c5211e455f3be54ffd6e62f23407114dd95f5
salith <salithanwar69@gmail.com>
2024-10-22 19:58:49 +0600

    Enable secure HTTP-only cookies for JWT token storage
    • Set httpOnly to true • Add sameSite, path, secure settings • Set expiration date for cookie • Remove commented-out code

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,11 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   })
-  // res.cookie('jwt', token, {
-  //   httpOnly: true,
-  //   sameSite: 'strict',
-  //   path: '/',
-  //   secure: true,
-  //   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-  // })
   res.cookie('jwt', token, {
-    httpOnly: false,
+    sameSite: 'strict',
+    path: '/',
+    secure: true,
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   })
   return token
 }
